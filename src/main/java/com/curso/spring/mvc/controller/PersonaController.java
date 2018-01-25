18e4c1e27854128b4ec9df6cadd65fd206685c4e
ngutierrez.rodriguez <ngutierrez.rodriguez@AT-C8MTN32.atsistemas.lan>
2018-01-25 16:17:18 +0100

    Muchas cosas de las que no me acuerdo. Jódete Natalia del futuro

--- src/main/java/com/curso/spring/mvc/controller/PersonaController.java
+++ src/main/java/com/curso/spring/mvc/controller/PersonaController.java
@@ -5,19 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.curso.spring.mvc.dto.Persona;
 import com.curso.spring.mvc.service.PersonaService;
 
-@Controller
+@RestController
 @RequestMapping("/characters")
 public class PersonaController {
 
@@ -25,29 +23,37 @@ public class PersonaController {
 	private PersonaService personaService;
 	
 	@RequestMapping(path = "/{id}", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody public Persona consultaPorId(String id) {
+	public Persona consultaPorId(@PathVariable String id) {
 		return personaService.buscarPersonaPorId(id);	
 	}
 	
 	@RequestMapping(path = "/{id}/father", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public Persona consultarPadre(String id) {
+	public Persona consultarPadre(@PathVariable String id) {
 		return personaService.consultarPadre(id);	
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public List<Persona> consultar(@RequestParam Persona persona) {
+	public List<Persona> consultarTodos() {
+		Persona persona = new Persona();
+		return personaService.buscarPersonas(persona);	
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE, params="apellido")
+	public List<Persona> consultarPorApellido(@PathVariable String apellido) {
+		Persona persona = new Persona();
+		persona.setApellido(apellido);
 		return personaService.buscarPersonas(persona);	
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(value = HttpStatus.CREATED)
-	public void addPersona(@RequestParam Persona persona) {
+	public void addPersona(@RequestBody Persona persona) {
 		personaService.addPersona(persona);	
 	}
 	
 	@RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
 	@ResponseStatus(value = HttpStatus.OK)
-	public void borrarPorId(String id) {
+	public void borrarPorId(@PathVariable String id) {
 		personaService.detelePersona(id);	
 	}
 }
